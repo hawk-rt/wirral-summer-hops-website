a25452f9b5d4424b58f3e1b58156ba77622d65a1
Chris Nolan <chris@leytech.co.uk>
2023-11-14 11:31:27 +0000

    Web flyer holding page

--- source/tickets.blade.php
+++ source/tickets.blade.php
@@ -8,7 +8,8 @@ url: https://www.trybooking.com/uk/events/landing/49231
 @section('body')
     <div class="container py-3">
         <div class="row text-center">
-            <h1>Your are being redirected to our ticket booking page... <span id="countdown">5</span></h1>
+            <h1>Wirral Summer Hops Beer Festival</h1>
+            <p class="lead">Your are being redirected to our ticket booking page... <span id="countdown">5</span></p>
             <p class="lead"><a href="{{ $page->url }}">Click here</a> if you are not redirected automatically.</p>
         </div>
     </div>
